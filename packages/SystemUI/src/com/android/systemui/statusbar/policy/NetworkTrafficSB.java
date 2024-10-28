c583c14c8fb463d1913b0d76805767147c8b899d
klozz <carlosj@klozz.dev>
2024-10-27 19:41:17 -0600

    Revert "NetworkTrafficSB: Better workaround for RequestLayout() spam"
    This reverts commit b28b99f8ec70eda40ac3129a9ce6089cc32c21f8.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -103,6 +103,7 @@ public class NetworkTrafficSB extends NetworkTraffic implements DarkReceiver, St
                 mSystemIconVisible = false;
                 break;
         }
+        update();
     }
 
     @Override
